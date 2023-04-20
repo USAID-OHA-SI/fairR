6e83d633eb1e89e016c495880a927e57c0995e3b
Prasann R <prasann@gwu.edu>
2023-04-20 14:06:17 -0400

    Finalizing equity plots

--- Scripts/01_equity_dumbbell_plot.R
+++ Scripts/01_equity_dumbbell_plot.R
@@ -7,6 +7,7 @@
 # DEPENDENCIES ------------------------------------------------------------
 
 library(tidyverse)
+library(dplyr)
 library(gagglr)
 library(mindthegap)
 library(glue)
@@ -34,7 +35,7 @@ df_est <- pull_unaids(orginal_unaids = TRUE, data_type = "HIV Estimates",
 #get USAID indicator numbers by sex
 df_tx <- df %>%
   filter(sex %in% c("Male", "Female"),
-         indicator %in% c("TX_CURR", "TX_NEW", "HTS_TST_POS"),
+         indicator %in% c("PrEP_CT", "HTS_TST_POS", "TX_NEW", "TX_PVLS", "TX_CURR"),
          country == "Guatemala",
          funding_agency == "USAID",
          fiscal_year == 2022) %>% 
@@ -58,26 +59,33 @@ df_plhiv <- df_est %>%
 # Duplicate df_plhiv to match df_tx
 df_plhiv <- df_plhiv %>%
   rbind(df_plhiv) %>%
+  rbind(df_plhiv) %>% 
+  rbind(df_plhiv) %>% 
   rbind(df_plhiv)
 
 # add in reference column for geom_segment
 df_tx <- df_tx %>% 
-  mutate(plhiv_freq = rev(df_plhiv$freq))
+  mutate(plhiv_freq = rev(df_plhiv$freq)) 
+
+# set indicator column to factors to reorder points
+df_tx$indicator = factor(df_tx$indicator, levels = c("TX_PVLS", "TX_CURR", "TX_NEW", "HTS_TST_POS", "PrEP_CT"))
 
 # VISUALIZE-----------------------------------------------------
 #visualize dumbbell plot and line segments
-df_viz <- ggplot(df_tx) + 
-  geom_vline(xintercept = df_plhiv$freq[1], linetype = "dotted", color = genoa) +
-  geom_vline(xintercept = df_plhiv$freq[2], linetype = "dotted", color = moody_blue) +
-  geom_point(aes(x=freq, y=indicator), size = 3, color = df_tx$sex_color, show.legend = TRUE) +
-  geom_text(aes(df_tx$freq,df_tx$indicator), label = round(df_tx$freq), size = 4, color = trolley_grey, nudge_y = .2, family = "Source Sans Pro") +
-  geom_segment(x = df_tx$plhiv_freq, xend = df_tx$freq, y = df_tx$indicator, yend = df_tx$indicator) +
+df_viz <- ggplot(df_tx) +
+  geom_point(aes(x=df_tx$freq, y=df_tx$indicator), size = 3, color = df_tx$sex_color, show.legend = TRUE) +
+  geom_text(aes(df_tx$freq, df_tx$indicator), label = round(df_tx$freq), size = 4, color = trolley_grey, nudge_y = .2, nudge_x = 3, family = "Source Sans Pro") +
+  geom_segment(x = df_tx$freq, xend = df_tx$plhiv_freq, y = df_tx$indicator, yend = df_tx$indicator, color = trolley_grey) +
+  
+  geom_vline(xintercept = df_tx$plhiv_freq[8], linetype = "dashed", color = genoa, size = 1) +
+  geom_vline(xintercept = df_tx$plhiv_freq[1], linetype = "dashed", color = moody_blue, size = 1) +
+  
   labs(title = glue("HTS_TST_POS DISPLAYS THE GREATEST DISPARITY BY SEX"),
        subtitle = glue("Indicator results and UNAIDS PLHIV estimates by  <span style = 'color: #8980cb;'>
                         Female</span> and <span style = 'color: #287c6f;'>Male</span>
                         in {df_tx$country[1]}"),
        caption = glue("Source: MER {metadata$curr_fy_lab}"),
-       x = "% Share of PLHIV by Sex",
+       x = "% Share of PLHIV by Sex (Dotted line indicates UNAIDS PLHIV Estimates)",
        y = "") +
   si_style_xgrid() +
   scale_x_continuous(breaks = seq(10, 90, by = 10)) +
@@ -88,7 +96,3 @@ plot(df_viz)
 #save plot
 si_save("Images/equity_sex_guatemala.png")
 si_save("Graphics/equity_sex_guatemala.svg")
-
-
-  
-  
--- Scripts/02_equity_facet_plot.R
+++ Scripts/02_equity_facet_plot.R
@@ -9,66 +9,92 @@
 library(tidyverse)
 library(gagglr)
 library(extrafont)
-library(scales)
 library(glue)
 library(ggtext)
 library(mindthegap)
+library(scales)
 
 # GLOBAL VARIABLES --------------------------------------------------------
 
-authors <- c("Prasann Ranade", "Nada Petrovic", "Karishma Srikanth")
-# ref_id <- "0530547f"
-# vrsn <- 1
+#authors <- c("Prasann Ranade", "Nada Petrovic", "Karishma Srikanth")
+#ref_id <- "0530547f"
+#vrsn <- 1
 
 # IMPORT ------------------------------------------------------------------
 
 df <- si_path() %>% 
-  return_latest("PSNU_IM_FY21-23") %>% 
+  return_latest("OU_IM_FY21-23") %>% 
   read_psd()  
+get_metadata()
+
+#pull unaids estimates
+df_est <- pull_unaids(orginal_unaids = TRUE, data_type = "HIV Estimates", 
+                      pepfar_only = TRUE)
 
 # MUNGE -------------------------------------------------------------------
 
+# filter to country, indicators, and disaggregates
+ind_sel <- c("TX_NEW", "TX_CURR", "TX_PVLS", "PrEP_NEW", "PrEP_CT")
+
 df_prep <- df %>% 
-  filter(standardizeddisaggregate %in% c("Age/Sex/HIVStatus", "KeyPop/HIVStatus"),
-         indicator == "TX_NEW",
-         funding_agency=="USAID",
-         country == "Guatemala",
-         fiscal_year >= 2021)
+  filter(country == "Guatemala")
 
 df_prep <- df_prep %>% 
+  filter(
+    standardizeddisaggregate %in% c("Age/Sex/HIVStatus", "KeyPop/HIVStatus"),
+    indicator %in% ind_sel,
+    funding_agency=="USAID",
+    fiscal_year >= 2021) 
+
+#ageasentered %in% c("35-39","40-44","45-49","50+") ~ "35+",
+df_prep <- df_prep %>% 
   # Combine age groups above 35+, rename People in Prisons
-  mutate(ageasentered = case_when(ageasentered %in% c("35-39","40-44","45-49","50+") ~ "35+", 
+  mutate(ageasentered = case_when(ageasentered %in% c("<01","01-04","05-09", "10-14") ~ "<15",
+                                  ageasentered %in% c("45-49","50-54","50+", "55-59", "60-64", "65+") ~ "45+", 
                                   TRUE ~ ageasentered),
          otherdisaggregate = case_when(otherdisaggregate %in% "People in prisons and other enclosed settings" ~ "Prisoners", 
                                        TRUE ~ otherdisaggregate)) %>%
   # Create index for faceting - either age or KP group
-  mutate(facet_ind=case_when(standardizeddisaggregate == "Age/Sex" ~ ageasentered, 
+  mutate(facet_ind=case_when(standardizeddisaggregate == "Age/Sex/HIVStatus" ~ ageasentered, 
                              TRUE ~ otherdisaggregate)) %>%
   filter(facet_ind!="Unknown Age") %>%
   # Fill sex indicator with "KP" value for KP - will be used for color
   mutate(sex = case_when(is.na(sex)==TRUE  ~ "KP", TRUE ~ sex)) %>%
   group_by(fiscal_year, country, funding_agency, indicator, standardizeddisaggregate, sex, ageasentered, otherdisaggregate, facet_ind) %>% 
   summarise(across(starts_with("qtr"), \(x) sum(x, na.rm = TRUE)),
-            .groups = "drop") %>% 
+            .groups = "drop"
+  ) %>% 
   reshape_msd() %>% 
   select(-period_type) %>% 
   arrange(period) 
+
+df_plhiv <- df_est %>% 
+  filter(indicator %in% c("Number PLHIV"),
+         country %in% c("Guatemala"),
+         year >= 2021) %>% 
+  group_by(indicator, sex, age, estimate) %>%  
+  ungroup() 
+
+# plot only one indicator at a time
+df_facet <- df_prep %>% 
+  filter(indicator == "TX_NEW") 
 
 # VISUALIZE --------------------------------------------------------------------
 
 pd_brks <- unique(df_prep$period) %>% str_replace(".*(2|4)$", "")
 
 # Add facets and labels
-df_viz <- df_prep %>% 
+df_viz <- df_facet %>% 
   ggplot(aes(x=period, y=value)) + 
   geom_point(aes(color=sex), size = 4, alpha = .8) +
-  geom_line(aes(group=sex, color=sex), linewidth=1, alpha = .8) +
+  geom_line(aes(group = sex, color=sex), linewidth=1, alpha = .8) +
   scale_x_discrete(labels = pd_brks) +
   scale_y_continuous(labels = comma) +
   scale_color_manual(values = c("Female" = moody_blue, "Male" = genoa, "KP"=burnt_sienna),
                      labels = function(x) str_to_upper(x)) +
-  facet_wrap(facet_ind ~ ., scales = "fixed", ncol=5, dir="h") +
-  si_style_yline() +
+  facet_wrap(facet_ind ~ ., scales = "fixed", ncol=4, dir="h") +
+  geom_hline(data = df_plhiv, aes(yintercept = estimate[4]/100), linetype = "dashed") +
+  si_style_xyline() +
   theme(strip.text.x = element_text(size = 12, face="bold"),
         axis.text.x = element_text(size=6),
         axis.text.y = element_text(size=10, angle=0))+
@@ -76,13 +102,16 @@ df_viz <- df_prep %>%
   theme(legend.position = "none") +
   coord_cartesian(clip="off") +
   labs(x = NULL, y = NULL,
-       title = glue("Is PrEP usage relatively consistent across demographics?"),
-       subtitle = glue("{unique(df_prep$funding_agency)}/{unique(df_prep$country)} {df_prep$indicator} by <span style = 'color: #8980cb;'>Female</span>, <span style = 'color: #287c6f;'>Male</span>
-     and <span style = 'color: #e07653;'>Key Populations</span> <br />"),
-       caption = glue("{metadata$caption} | USAID/OHA/SIEI |  Ref ID: {ref_id} v{vrsn}"))
+       title = glue("Are treatment numbers relatively consistent across demographics?"),
+       subtitle = glue("USAID/Guatemala TX_NEW 
+                      by <span style = 'color: #8980cb'>Female</span>, 
+                      <span style = 'color: #287c6f;'>Male</span>, and
+                      <span style = 'color: #e07653;'>Key Populations</span>"),
+       caption = glue("Dashed line indicates UNAIDS PLHIV Estimates \n
+                      {metadata$caption} | USAID/OHA/SIEI"))
 
 plot(df_viz)
-  
+
 #save plot
-si_save("Images/equity_disagg_guatemala.png")
+si_save("Images/equity_disagg_guatemala_tx_new.png")
 si_save("Graphics/equity_disagg_guatemala.svg")
